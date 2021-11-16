dd781f3a87002c7817a8a913a23c742ef0953665
unknown <kovacsdominik63@gmail.com>
2021-11-16 13:15:27 +0100

    Hatodik mentés

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -15,3 +15,10 @@ for x in szamok:
     if (x==1):
         vane=True
 print(vane)
+   
+#megszámlálás tétele hány darab 3as van a listában
+db=0
+for x in szamok:
+    if x==3 :
+        db=db+1;
+print(db)
