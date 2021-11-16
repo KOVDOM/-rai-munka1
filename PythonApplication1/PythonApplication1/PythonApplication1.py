8753f0b25b6c29898d2d4dbcd926e5a4163d8bcb
unknown <kovacsdominik63@gmail.com>
2021-11-16 12:10:36 +0100

    Negyedik mentés

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,17 +1,11 @@
 print("Működik a Git?")
 print("Mindjárt kiderül")
-#Írjon programot, mely kiírja a képernyőre a páros számokat 1-től 100-ig.
-for i in range(0, 101, 2): 
-    print(i)
-    if (i%2==1):
-        print(i)
 
-#Olvass be két számot, írd ki a négyzetüket!
-szam1=int(input("Kérek egy szabadon választott számot! "))
-szam2=int(input("Mivel egy szám kevés ezért kérek még egy szabadon választott számot! "))
-eredmeny=szam1**szam2
-print(eredmeny)
+szamok=[3,4,2,7,8,1,9,7,3]
 
-#Írjon programot, mely kiírja a képernyőre a páros számokat 50-től 10-ig.
-for i in range(50, 9, -2): 
-    print(i)
+#összegzés tétele
+osszeg =0
+for x in szamok:
+    osszeg=osszeg+x
+    print(osszeg)
+print(osszeg)
